ad3bd3c59e077bfae9b4ba21c6b8a0879c63088c
song0331 <thdgusrb@gmail.com>
2024-01-28 13:49:01 +0900

    [add] 과제2 절대경로 설정

--- homework_2/vite.config.js
+++ homework_2/vite.config.js
@@ -1,7 +1,22 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'ProductList': path.resolve(__dirname, 'src/components/ProductList'),
+      'ProductListSort': path.resolve(__dirname, 'src/components/ProductListSort'),
+      'Product': path.resolve(__dirname, 'src/components/Product'),
+      'Pagination': path.resolve(__dirname, 'src/components/Pagination'),
+      'data': path.resolve(__dirname, 'src/data'),
+      'images': path.resolve(__dirname, 'src/assets/images'),
+    }
+  },
+  css: {
+    devSourcemap: true
+  }
 })
