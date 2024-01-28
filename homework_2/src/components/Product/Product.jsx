5f1adce757e35b805160ab837f12e85578f5aab5
song0331 <thdgusrb@gmail.com>
2024-01-28 21:27:43 +0900

    [modify] 과제2 이미지 주소에 사용된 index를 id값으로 변경

--- homework_2/src/components/Product/Product.jsx
+++ homework_2/src/components/Product/Product.jsx
@@ -6,11 +6,11 @@ export default function Product() {
   return (
     <ul className='Product_wrapper'>
       {
-        product_data.map((item, index) => {
+        product_data.map((item) => {
           return (
             <li key={item.id} className="product">
               <a href="#">
-                <img src={`/images/${index + 1}.jpg`} alt={item.product_name} />
+                <img src={`/images/${item.id + 1}.jpg`} alt={item.product_name} />
                 <div className='product_info'>
                   <span className='delivery_type'>{item.delivery_type}</span>
                   <h3 className='product_name'>{item.product_name}</h3>
