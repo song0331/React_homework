c2e8788817940b6cd4ae38b70afdd544054a37fe
song0331 <thdgusrb@gmail.com>
2024-02-13 01:18:05 +0900

    [ add ] 과제4 포켓베이스와 연결해서 데이터 가져오기

--- homework_4/src/components/ChatList.jsx
+++ homework_4/src/components/ChatList.jsx
@@ -1,17 +1,55 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from './../styles/ChatList.module.css'
+import PocketBase from 'pocketbase';
+import { useContext, useEffect, useState } from 'react';
+import StatusBar from './StatusBar';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 export default function ChatList() {
 
-  const name = ['민지', '하니', '다니엘', '해린', '혜인']
+  const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
+  const theme = useContext(ThemeContext);
+  const style = `${styles.container} ${theme}`
+  const user = JSON.parse(localStorage.getItem('pocketbase_auth')).model.name;
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState([]);
+  const name = ['민지', '하니', '다니엘', '해린', '혜인', '리액트'];
+
+  // console.log(user)
+  const handleLogout = () => {
+    pb.authStore.clear();
+    navigate('/');
+  }
+
+  useEffect(() => {
+    pb.collection('message').getFullList()
+      .then((data) => {
+        // console.log(data);
+        // console.log(data[0].receiver);
+
+        // const test = data.map((item, index) => {
+        //   if (item.receiver === user) return
+        // })
+
+
+        // console.log(data[0].sender);
+        // console.log(data[0].message[data[0].message.length - 1]);
+        setUserData([...userData, ...data]);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, [])
+
 
   return (
-    <div className={styles.container}>
-      <div className={styles.status_bar}>
-        <img src="/icons/left.svg" alt="시간" />
-        <img src="/icons/right.svg" alt="와이파이, 통신, 배터리 아이콘" />
+    <div className={style}>
+      {/* <div className={styles.container}> */}
+      <StatusBar />
+      <div className={styles.user_name}>
+        {user} 님
+        <button type='button' onClick={handleLogout}>Logout</button>
       </div>
-
       <div className={styles.menu}>
         <h2>채팅</h2>
         <div className={styles.icons}>
@@ -22,18 +60,32 @@ export default function ChatList() {
       </div>
 
       {
-        name.map((item, index) => {
+        userData.map((item) => {
           return (
-            <Link to='/chatRoom' key={index}>
+            <Link to={`/chatRoom/${item.id}`} key={crypto.randomUUID()}>
               <div className={styles.chat_list}>
-                <img src={`/images/${item}.svg`} alt="대화 상대 프로필 사진" />
+
+                {
+                  name.includes(item.sender) ?
+                    <img src={`/images/${item.sender}.svg`} alt="대화 상대 프로필 사진" />
+                    : <img src={`/images/profile.svg`} alt="대화 상대 프로필 사진" />
+                }
 
                 <div className={styles.profile_wrapper}>
                   <div role='none'>
-                    <span className={styles.name}>{item}</span>
-                    <p className={styles.message}>곧 컴백이야!</p>
+                    <span className={styles.name}>{item.sender}</span>
+                    {
+                      Object.keys(item.message[item.message.length - 1])[0] === item.sender ?
+                        <p className={styles.message}>
+                          {item.message[item.message.length - 1][item.sender]}
+                        </p>
+                        :
+                        <p className={styles.message}>
+                          {item.message[item.message.length - 1][item.receiver]}
+                        </p>
+                    }
                   </div>
-                  <time className={styles.time}>20:05</time>
+                  <time className={styles.time}>{item.updated.slice(11, -8)}</time>
                 </div>
               </div>
             </Link>
--- homework_4/src/components/ChatRoom.jsx
+++ homework_4/src/components/ChatRoom.jsx
@@ -1,20 +1,85 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import styles from './../styles/ChatRoom.module.css'
+import { useEffect, useRef, useState } from 'react'
+import PocketBase from 'pocketbase';
+import StatusBar from './StatusBar';
 
 export default function ChatRoom() {
 
+  const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
+  const [sendMessage, setSendMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+  const [sender, setSender] = useState('');
+  const { id } = useParams();
+  const inputRef = useRef(null);
+  const scrollRef = useRef(null);
+  const name = ['민지', '하니', '다니엘', '해린', '혜인', '리액트'];
+
+  const handleMessage = (e) => {
+    setSendMessage(e.target.value)
+  }
+  const handleSendMessage = () => {
+    setSendMessage('')
+    inputRef.current.focus()
+
+    // scrollRef.current.scrollTop = scrollRef.current.scrollHeight + 40;
+    // console.log(scrollRef.current.scrollHeight)
+    // console.log(typeof scrollRef.current.scrollHeight)
+
+    const data = {
+      "message": JSON.stringify([...messageList, { '현규': sendMessage }]),
+    };
+
+    pb.collection('message').update(id, data)
+      .then((data) => {
+        // console.log(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+  }
+
+
+  useEffect(() => {
+    inputRef.current.focus()
+
+    pb.collection('message').getOne(id)
+      .then((data) => {
+        // console.log(data.message)
+        setMessageList([...messageList, ...data.message]);
+        setSender(data.sender);
+      })
+      .catch((error) => {
+        console.log('실패', error)
+      })
+  }, [])
+
+
+  useEffect(() => {
+    pb.collection('message').subscribe(id, function (e) {
+      // console.log(e.action);
+      // console.log(e.record);
+
+      setMessageList([...messageList, ...e.record.message]);
+    })
+      // .then((data) => console.log('성공', data))
+      .catch(() => console.log('실패'))
+
+    return () => {
+      pb.collection('message').unsubscribe(id)
+    }
+
+  }, [])
+
+
   return (
     <div className={styles.container}>
-
-      <div className={styles.status_bar}>
-        <img src="/icons/left.svg" alt="시간" />
-        <img src="/icons/right.svg" alt="와이파이, 통신, 배터리 아이콘" />
-      </div>
-
+      <StatusBar />
       <div className={styles.menu}>
         <div role='none' className={styles.wrapper}>
           <Link to='/chatList'><img src="/icons/previous.svg" alt="뒤로 가기" /></Link>
-          <h2>민지</h2>
+          <h2>{sender}</h2>
         </div>
         <div className={styles.icons}>
           <img src="/icons/search.svg" alt="채팅 내용 검색" />
@@ -22,32 +87,50 @@ export default function ChatRoom() {
         </div>
       </div>
 
-      <div className={styles.receive_message}>
-        <img src='/images/민지.svg' alt="대화 상대 프로필 사진" />
-        <div role='none'>
-          <span>민지</span>
-          <p className={styles.speech_bubble}>
-            나 곧 컴백이야!
-          </p>
-        </div>
-      </div>
-
-      <div className={styles.send_message}>
-        <p className={styles.speech_bubble}>
-          축하해! 벌써 기대된다
-        </p>
+      <div ref={scrollRef} className={styles.message_container}>
+        {
+          messageList.length === 0 ? null
+            : messageList.map((item) => {
+              return (
+                <>
+                  <div key={crypto.randomUUID()}>
+                    {
+                      Object.keys(item)[0] === sender ?
+                        <div key={crypto.randomUUID()} className={styles.receive_message}>
+                          {
+                            name.includes(sender) ?
+                              <img src={`/images/${sender}.svg`} alt="대화 상대 프로필 사진" />
+                              : <img src={`/images/profile.svg`} alt="대화 상대 프로필 사진" />
+                          }
+                          <div role='none'>
+                            <span>{sender}</span>
+                            <p className={styles.speech_bubble}>
+                              {item[Object.keys(item)[0]]}
+                            </p>
+                          </div>
+                        </div>
+                        :
+                        <div key={crypto.randomUUID()} className={styles.send_message}>
+                          <p className={styles.speech_bubble}>
+                            {item[Object.keys(item)[0]]}
+                          </p>
+                        </div>
+                    }
+                  </div>
+                </>
+              )
+            })
+        }
       </div>
 
       <div className={styles.chatInput}>
         <label className='sr_only' htmlFor="message">메세지 입력창</label>
-        <input type="text" id='message' />
-        <button type='button' onClick={() => console.log('메세지 보냄')}>
+        <input ref={inputRef} type="text" id='message' value={sendMessage} onChange={handleMessage} />
+        <button type='button' onClick={handleSendMessage}>
           <img src="/icons/send.png" alt="메세지 보내기 버튼" />
         </button>
       </div>
 
-
-
-    </div>
+    </div >
   )
 }
